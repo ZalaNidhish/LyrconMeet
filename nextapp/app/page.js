380ce7dad6a0cd59f5bd17fd0d7cdfae291ea2c6
Nidhish Zala <nidhish1132007@gmail.com>
2026-03-31 00:55:48 +0530

    setup

---
+++ nextapp/app/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-neutral-100 h-screen w-screen text-black flex items-center justify-center gap-10 flex-col'>
+      Schedule a Meet for Lyrcon Solutions.
+      <div className='bg-indigo-400 cursor-pointer rounded-2xl p-4 text-white'>
+        Schedule a Meet.
+      </div>
+    </div>
+  )
+}
+
+export default page
