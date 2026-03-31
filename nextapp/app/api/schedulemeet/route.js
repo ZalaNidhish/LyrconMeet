bf2426c02fd7c96e77b6157367c9d6f15bbb5364
Nidhish Zala <nidhish1132007@gmail.com>
2026-03-31 13:38:00 +0530

    no repeated booking in same solt feature

--- nextapp/app/api/schedulemeet/route.js
+++ nextapp/app/api/schedulemeet/route.js
@@ -3,6 +3,12 @@ const meetingmodel = require("../../../models/meetings");
 export async function POST(req){
     const {fullname, email, phone, date, time} = await req.json();
 
+    const existingMeeting = await meetingmodel.findOne({date, time});
+
+    if(existingMeeting){
+        return new Response(JSON.stringify({message: "Meeting slot already booked"}), {status: 400});
+    }
+
     try {
         const newMeeting = new meetingmodel({
             fullname,
